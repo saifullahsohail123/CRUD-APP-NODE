77c9bfdeff4990e964f2c66f593be8db9ee2a82c
Saif Ullah <saifu0905@gmail.com>
2024-11-25 11:49:31 +0500

    using express backend server

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('Hellow World')
+const express = require('express')
+const app = express()
+// Using Node.js `require()`
+
+
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+
+
+
+  app.listen(3000, console.log('Connected to localhost port 3000')), 
+
+
+
+
