e961f0dde56b82b7a62f069b889afbefc51901bd
Saif Ullah <saifu0905@gmail.com>
2024-11-26 09:38:00 +0500

    Added Database connection using mongoose, added the connection string

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 // Using Node.js `require()`
+const mongoose = require('mongoose');
+
 
 
 
@@ -11,8 +13,16 @@ app.get('/', function (req, res) {
 
 
 
-  app.listen(3000, console.log('Connected to localhost port 3000')), 
 
-
-
+mongoose
+  .connect('mongodb+srv://admin:admin@cluster0.lstf4.mongodb.net/node-api?retryWrites=true&w=majority&appName=Cluster0')
+  .then(() => {
+    console.log('Connected to Database');
+    app.listen(3000, () => {
+      console.log('Connected to localhost port 3000');
+    });
+  })
+  .catch((err) => {
+    console.error('Database connection error:', err);
+  });
 
